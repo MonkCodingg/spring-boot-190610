651638e947fdd1aa23783d89d9eb671ef1241b82
laevus.j <laevus.j@gmail.com>
2019-06-11 20:56:03 +0900

    두번째 커밋

--- java/com/bitcamp/web/controller/HomeController.java
+++ java/com/bitcamp/web/controller/HomeController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller //어노테이션사용 빈(bean) 등록. 없으면 클래스 상태. 있으면 컨트롤러(역할) 빈이면서 클래스.
 public class HomeController {   // 클래스를 선언했다.
     
-    @RequestMapping("/") //mapping 매칭
+    @RequestMapping("/") //application.properties 의 prefix에 맵핑
     public String index() {
         System.out.println("루트 URL 경로로 들어왔음");
         
-        return "index";
+        return "index"; //application.properties 의 suffix에 맵핑
     } 
         
 }
