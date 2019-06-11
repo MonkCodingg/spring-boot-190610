f7e889984040d78dda0ba3939666979585aa7097
laevus.j <laevus.j@gmail.com>
2019-06-11 17:33:11 +0900

    타이타닉

---
+++ java/com/bitcamp/web/WebApplication.java
@@ -0,0 +1,13 @@
+package com.bitcamp.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication { //real 코드
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args); //가상코드. 컴파일된 이후 jvm(가상머신)이 읽어드린다
+	}
+
+}
