6d089aa04a63c6c59737e3498c38953046e3413a
laevus.j <laevus.j@gmail.com>
2019-06-12 10:35:25 +0900

    이파이패턴

--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -1,22 +1,28 @@
-alert("자바스크립트 연결 성공 !!");
 
-var wrapper = document.querySelector('#wrapper'); //wrapper 객체 '#'=id,'.'=class 
-// bom > dom > 컴퍼넌트
 
-wrapper.innerHTML = '<h1>spa 시작</h1>' //컴퍼넌트에 html 집어넣기
-+'<div id="target">삭제할 내용 </div>'
-+'<button id="btn">지워버리자</button>'
-+'<button id="btn2">추가하자</button>';
+//var app;  //expression이 아님. declaration임
+//IIFE(이파이) 패턴
+var app = (function(){
+    alert("자바스크립트 연결 성공 !!");
 
-var btn = document.querySelector('#btn'); //id로 접근해서 변수 받아오기
-var target = document.querySelector('#target'); //id로 접근해서 변수 받아오기
-btn.addEventListener('click', function(){   // 버튼에 이벤트 
-    //alert('버튼 클릭');
-    //wrapper.innerHTML =''; // wrapper 컴퍼넌트에 html 지우기
-    target.innerHTML='';    // target 컴퍼넌트에 html 지우기
-});
-var btn2 = document.querySelector('#btn2');
-btn2.addEventListener('click', function(){
-    var temp = document.createTextNode('새로 추가됨');   // 
-    target.appendChild(temp);
-});
+    var wrapper = document.querySelector('#wrapper'); //wrapper 객체 '#'=id,'.'=class 
+    // bom > dom > 컴퍼넌트
+
+    wrapper.innerHTML = '<h1>spa 시작</h1>' //컴퍼넌트에 html 집어넣기
+    +'<div id="target">삭제할 내용 </div>'
+    +'<button id="btn">지워버리자</button>'
+    +'<button id="btn2">추가하자</button>';
+
+    var btn = document.querySelector('#btn'); //id로 접근해서 변수 받아오기
+    var target = document.querySelector('#target'); //id로 접근해서 변수 받아오기
+    btn.addEventListener('click', function(){   // 버튼에 이벤트 걸기
+        //alert('버튼 클릭');
+        //wrapper.innerHTML =''; // wrapper 컴퍼넌트에 html 지우기
+        target.innerHTML='';    // target 컴퍼넌트에 html 지우기
+    });
+    var btn2 = document.querySelector('#btn2');
+    btn2.addEventListener('click', function(){
+        var temp = document.createTextNode('새로 추가됨');   // 
+        target.appendChild(temp);
+    });
+})(); // '자바스크립트 객체'라 부름. DOM객체가 아님.
