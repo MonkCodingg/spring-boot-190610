3b85741f763d0e0866ccdb0329acc439fbd94211
laevus.j <laevus.j@gmail.com>
2019-06-12 14:04:10 +0900

    14:03

--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -1,28 +1,69 @@
-
-
 //var app;  //expression이 아님. declaration임
-//IIFE(이파이) 패턴
-var app = (function(){
-    alert("자바스크립트 연결 성공 !!");
 
-    var wrapper = document.querySelector('#wrapper'); //wrapper 객체 '#'=id,'.'=class 
-    // bom > dom > 컴퍼넌트
+var app = (function(){//IIFE(이파이) 패턴
+    let init=function(){ // let 블럭 스코프에 대응한 변수 선언.
+        alert("IIFE 성공 !!");
+        login_form();
+        
+    }
+    //시블링
+    let login_form = function(){
+        var wrapper = document.querySelector('#wrapper'); //wrapper 객체 '#'=id,'.'=class 
+        // bom > dom > 컴퍼넌트
+        wrapper.innerHTML = +'<form action="/action_page.php">'
+        +'  First name:<br>'
+        +'  <input type="text" name="firstname" value="Mickey">'
+        +'  <br>'
+        +'  Last name:<br>'
+        +'  <input type="text" name="lastname" value="Mouse">'
+        +'  <br><br>'
+        +'  <input type="button" id="login" value="로그인">'
+        +'  <input type="button" id="signUp" value="회원가입">'
+        +'</form> ';
 
-    wrapper.innerHTML = '<h1>spa 시작</h1>' //컴퍼넌트에 html 집어넣기
-    +'<div id="target">삭제할 내용 </div>'
-    +'<button id="btn">지워버리자</button>'
-    +'<button id="btn2">추가하자</button>';
+        var login = document.querySelector('#login'); //id로 접근해서 변수 받아오기
+        var signUp = document.querySelector('#signUp'); //id로 접근해서 변수 받아오기
+        login.addEventListener('click', function(){   // 버튼에 이벤트 걸기
+            alert('로그인 클릭');
+            //wrapper.innerHTML =''; // wrapper 컴퍼넌트에 html 지우기
+ //           wrapper.innerHTML='로그인 번튼 후 이동';    // target 컴퍼넌트에 html 지우기
+        });
+        
+        signUp.addEventListener('click', function(){
+            alert('회원가입 클릭');
+            //wrapper 컴퍼넌트에 회원가입 폼 html 넣기
+            wrapper.innerHTML=+'<form action="/action_page.php">'
+            +'  id:<br>'
+            +'  <input type="text" name="id" >'
+            +'  <br>'
+            +'  pass:<br>'
+            +'  <input type="text" name="pass" >'
+            +'  <br>'
+            +'  name:<br>'
+            +'  <input type="text" name="home" >'
+            +'  <br>'
+            +'  ssn:<br>'
+            +'  <input type="text" name="ssn" >'
+            +'  <br>'
+            +'  phone:<br>'
+            +'  <input type="text" name="phone" >'
+            +'  <br>'
+            +'  <input type="button" id="signupOk" value="회원가입">'
+            +'  <input type="button" id="cancle" value="취소"> '
+            +'</form>';
 
-    var btn = document.querySelector('#btn'); //id로 접근해서 변수 받아오기
-    var target = document.querySelector('#target'); //id로 접근해서 변수 받아오기
-    btn.addEventListener('click', function(){   // 버튼에 이벤트 걸기
-        //alert('버튼 클릭');
-        //wrapper.innerHTML =''; // wrapper 컴퍼넌트에 html 지우기
-        target.innerHTML='';    // target 컴퍼넌트에 html 지우기
-    });
-    var btn2 = document.querySelector('#btn2');
-    btn2.addEventListener('click', function(){
-        var temp = document.createTextNode('새로 추가됨');   // 
-        target.appendChild(temp);
-    });
+            var signupOK = document.querySelector('#signupOk');
+            var cancle = document.querySelector('#cancle');
+            signupOk.addEventListener('click', function(){   // 버튼에 이벤트 걸기
+                alert('회원가입 완료 클릭');
+//                wrapper.innerHTML =''; // wrapper 컴퍼넌트에 html 지우기
+//                wrapper.innerHTML='로그인 번튼 후 이동';    // target 컴퍼넌트에 html 지우기
+            });
+ //           cancle.addEventListener()
+        });
+    }
+    return {init : init}; // <--클로저. function은 인풋, 아웃풋 
+    //맨마지막 단계 -> 클로저(마무리)라고 한다.
+    //init의 값을app에 전달해 준다.
+    //json 
 })(); // '자바스크립트 객체'라 부름. DOM객체가 아님.
