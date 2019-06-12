5dc228b7a2c6d0cd8ebc6bcf86f2379458bd11a1
laevus.j <laevus.j@gmail.com>
2019-06-12 14:30:26 +0900

    14:30

--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -17,50 +17,33 @@ var app = (function(){//IIFE(이파이) 패턴
         +'  Last name:<br>'
         +'  <input type="text" name="lastname" value="Mouse">'
         +'  <br><br>'
-        +'  <input type="button" id="login" value="로그인">'
-        +'  <input type="button" id="signUp" value="회원가입">'
+        +'  <input type="button" id="login-btn" value="로그인">'
+        +'  <input type="button" id="join-btn" value="회원가입">'
         +'</form> ';
 
-        var login = document.querySelector('#login'); //id로 접근해서 변수 받아오기
-        var signUp = document.querySelector('#signUp'); //id로 접근해서 변수 받아오기
-        login.addEventListener('click', function(){   // 버튼에 이벤트 걸기
-            alert('로그인 클릭');
-            //wrapper.innerHTML =''; // wrapper 컴퍼넌트에 html 지우기
- //           wrapper.innerHTML='로그인 번튼 후 이동';    // target 컴퍼넌트에 html 지우기
-        });
         
-        signUp.addEventListener('click', function(){
-            alert('회원가입 클릭');
-            //wrapper 컴퍼넌트에 회원가입 폼 html 넣기
-            wrapper.innerHTML=+'<form action="/action_page.php">'
-            +'  id:<br>'
-            +'  <input type="text" name="id" >'
-            +'  <br>'
-            +'  pass:<br>'
-            +'  <input type="text" name="pass" >'
-            +'  <br>'
-            +'  name:<br>'
-            +'  <input type="text" name="home" >'
-            +'  <br>'
-            +'  ssn:<br>'
-            +'  <input type="text" name="ssn" >'
-            +'  <br>'
-            +'  phone:<br>'
-            +'  <input type="text" name="phone" >'
-            +'  <br>'
-            +'  <input type="button" id="signupOk" value="회원가입">'
-            +'  <input type="button" id="cancle" value="취소"> '
-            +'</form>';
-
-            var signupOK = document.querySelector('#signupOk');
-            var cancle = document.querySelector('#cancle');
-            signupOk.addEventListener('click', function(){   // 버튼에 이벤트 걸기
-                alert('회원가입 완료 클릭');
-//                wrapper.innerHTML =''; // wrapper 컴퍼넌트에 html 지우기
-//                wrapper.innerHTML='로그인 번튼 후 이동';    // target 컴퍼넌트에 html 지우기
+            let join_btn = document.querySelector('#join-btn');
+            join_btn.addEventListener('click', function(){
+                join_form();
             });
- //           cancle.addEventListener()
-        });
+            
+        let join_form = function(){
+            let wrapper = document.querySelector('#wrapper');
+            wrapper.innerHTML = '<form>'
+            +'	ID<br>'
+            +'	<input type="text" name="id"><br>'
+            +'	PW<br>'
+            +'	<input type="password" name="pw"><br>'
+            +'	이름<br>'
+            +'	<input type="text" name="name"><br>'
+            +'	주민번호<br>'
+            +'	<input type="password" name="ssn"><br>'
+            +'	전화번호<br>'
+            +'	<input type="text" name="phone"><br><br>'
+            +'	<input id="btn3" type="submit" value="확인">'
+            +'	<input id="btn4" type="reset" value="취소">'
+            +'</form>';
+        }
     }
     return {init : init}; // <--클로저. function은 인풋, 아웃풋 
     //맨마지막 단계 -> 클로저(마무리)라고 한다.
